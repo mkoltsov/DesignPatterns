52038f0031abe5d4031a48f4ef1e3bd8534ac6a4
mkoltsov <root@javabean.ru>
2015-05-19 18:17:37 +0200

    compound continued

--- compound/compound.groovy
+++ compound/compound.groovy
@@ -19,11 +19,54 @@ class DuckCall implements Quackable {
 	def void quack(){println('Kwak')}
 }
 
-void simulate(){
-	def mDuck = new MallardDuck()
-	def rDuck = new RedHeadDuck()
-	def rbDuck = new RubberDuck()
-	def dcDuck = new DuckCall()
+@Canonical
+class GooseAdapter implements Quackable {
+	def Goose goose
+
+	def void quack(){
+		goose.honk()
+	} 
+}
+
+class QuackCounter implements Quackable {
+	def Quackable duck
+	def static int quackCounter
+	QuackCounter(Quackable duck){
+		this.duck = duck
+	}
+	def void quack(){
+		duck.quack()
+		quackCounter++
+	}
+}
+
+interface AbstractDuckFactory {
+	def Quackable createMallardDuck()
+	def Quackable createRedheadDuck()
+	def Quackable createRubberDuck()
+	def Quackable createDuckCall()
+}
+
+class DuckFactory implements AbstractDuckFactory {
+	def Quackable createMallardDuck(){ return new MallardDuck()}
+	def Quackable createRedheadDuck(){return new RedHeadDuck()}
+	def Quackable createRubberDuck(){return new RubberDuck()}
+	def Quackable createDuckCall(){return new DuckCall()}
+}
+
+class CountingDuckFactory implements AbstractDuckFactory {
+	def Quackable createMallardDuck(){ return new QuackCounter(new MallardDuck())}
+	def Quackable createRedheadDuck(){return new QuackCounter( new RedHeadDuck())}
+	def Quackable createRubberDuck(){return new QuackCounter( new RubberDuck())}
+	def Quackable createDuckCall(){return new QuackCounter( new DuckCall())}
+}
+
+
+void simulate(AbstractDuckFactory countFactory){
+	def mDuck = countFactory.createMallardDuck()
+	def rDuck = countFactory.createRedheadDuck()
+	def rbDuck = countFactory.createRubberDuck()
+	def dcDuck = countFactory.createDuckCall()
 	def goose = new Goose()
 
 	simulate(mDuck)
@@ -31,6 +74,8 @@ void simulate(){
 	simulate(rbDuck)
 	simulate(dcDuck)
 	simulate(new GooseAdapter(goose))
+
+	println("number of ducks quacked is ${QuackCounter.quackCounter}")
 
 }
 
@@ -44,12 +89,7 @@ class Goose {
 	}
 }
 
-@Canonical
-class GooseAdapter implements Quackable {
-	def Goose goose
 
-	def void quack(){
-		goose.honk()
-	} 
-}
-simulate()
+
+
+simulate(new CountingDuckFactory())

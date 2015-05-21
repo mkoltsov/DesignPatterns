4354217e2b3861f2edb6e1cde7428702ba5b6e8b
mkoltsov <root@javabean.ru>
2015-05-21 19:45:30 +0700

    proxy added

---
+++ proxy/proxy.groovy
@@ -0,0 +1,25 @@
+import groovy.transform.Canonical
+interface PersonBean {
+	String getname()
+	String getGender()
+	String getInterests()
+	int getHotOrNotRating()
+
+	void setName(String name)
+	void setGender(String gender)
+	void setInterests(String interests)
+	void setHotOrNotRating(int rating)
+}
+
+@Canonical
+class PersonBeanImpl implements PersonBean {
+	def String name
+	def String gender
+	def String interests
+	int hotOrNotRating
+
+	def getHotOrNotRating(){
+
+	}
+
+}

36de89ec0b2e8c448034b96956d33b6ee0f3f65a
mkoltsov <root@javabean.ru>
2015-06-14 21:05:40 +0200

    adapter added

--- adapter/adapter.groovy
+++ adapter/adapter.groovy
@@ -1,19 +1,48 @@
-class EnumerationAdapter implements Iterator {
-	def enumeration
-	
-	EnumerationAdapter(Enumeration enumeration){
-		this.enumeration = enumeration
-	}
-
-	def boolean hasNext(){
-		return enumeration.hasMoreElements()
-	}
-
-	def void remove(){
- 		throw new UnsupportedOperationException()
-	}
-
-	def Object next(){
-		return enumeration.nextElement()
-	}
+interface Duck {
+	def quack()
+	def fly()
 }
+	
+interface Turkey {
+	def gobble()
+	def jump()
+}
+	
+class MallardDuck implements Duck {
+  def quack() {
+	println('Quacking...')
+  }
+  def fly() {
+	println('Flying...')
+  }
+}
+
+class TurkeyAdapter implements Duck {
+  def turkey
+  TurkeyAdapter(Turkey turkey){
+	this.turkey = turkey
+  }
+  
+  def quack(){
+     turkey.gobble()
+  }
+  def fly(){
+      println("i don't do much flying, instead i ${turkey.jump()}")
+  }
+  
+}
+
+class TGTurkey implements Turkey {
+  def gobble() {
+	println('Gobb...')
+  }
+  def jump() {
+	println('Yay!')
+  }
+}
+
+def testDuck = {_ -> _.fly()}
+def turkeyAdapter = new TurkeyAdapter(new TGTurkey())
+testDuck(turkeyAdapter)
+testDuck(new MallardDuck())
+//def turkeyAdapter = new TurkeyAdapter(new TGTurkey()).fly()

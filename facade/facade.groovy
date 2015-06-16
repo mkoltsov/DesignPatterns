604d819ea039a2cd25ff82f8dd68eeaa7eb1f349
mkoltsov <root@javabean.ru>
2015-06-16 21:07:45 +0200

    facade added

---
+++ facade/facade.groovy
@@ -0,0 +1,28 @@
+class HomeTheaterFacade {
+  def amp, popper, dvd, tv
+  HomeTheaterFacade(amp, popper, dvd, tv) {
+	this.amp = amp
+	this.popper = popper
+	this.dvd = dvd
+	this.tv = tv
+  }
+  
+  def watchMovie(){
+	amp.turnOn()
+	popper.pop()
+	dvd.turnOn()
+	tv.switchOn()
+	dvd.selectTheMovie()
+	dvd.playTheMovie()
+  }
+  
+  def switchOffMovie(){
+	amp.turnOff()
+	popper.turnOff()
+	dvd.turnOff()
+	tv.turnOff()
+  }
+}
+
+def theater = new HomeTheaterFacade(new Expando(), new Expando(), new Expando(), new Expando())
+theater.watchMovie()

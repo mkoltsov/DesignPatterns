011108c02ab62b990e7f4a42ee315e74c51628e0
mkoltsov <root@javabean.ru>
2015-06-17 21:09:12 +0200

    template method added

---
+++ templateMethod/templateMethod.groovy
@@ -0,0 +1,33 @@
+abstract class Beverage {
+
+	def prepareTheBeverage() {
+		boiling()
+		addCondiments()
+		addMilk()
+	}
+	
+	def abstract addCondiments()
+	
+	def addMilk(){
+		println('Adding milk')
+	}
+	
+	def boiling(){
+		println('Boiling the water')
+	}	
+}
+
+class Coffee extends Beverage {
+	def addCondiments(){
+		println('Adding coffee')
+	}
+}
+
+class Tea extends Beverage {
+	def addCondiments(){
+		println('Adding tea')
+	}
+}
+
+new Coffee().prepareTheBeverage()
+new Tea().prepareTheBeverage()

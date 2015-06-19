bfce84936eaf4c49eb3aab098a4d238921ab0714
mkoltsov <root@javabean.ru>
2015-06-19 22:12:16 +0200

    formatting

--- facade/facade.groovy
+++ facade/facade.groovy
@@ -1,27 +1,27 @@
 class HomeTheaterFacade {
-  def amp, popper, dvd, tv
-  HomeTheaterFacade(amp, popper, dvd, tv) {
-	this.amp = amp
-	this.popper = popper
-	this.dvd = dvd
-	this.tv = tv
-  }
-  
-  def watchMovie(){
-	amp.turnOn()
-	popper.pop()
-	dvd.turnOn()
-	tv.switchOn()
-	dvd.selectTheMovie()
-	dvd.playTheMovie()
-  }
-  
-  def switchOffMovie(){
-	amp.turnOff()
-	popper.turnOff()
-	dvd.turnOff()
-	tv.turnOff()
-  }
+	def amp, popper, dvd, tv
+	HomeTheaterFacade(amp, popper, dvd, tv) {
+		this.amp = amp
+		this.popper = popper
+		this.dvd = dvd
+		this.tv = tv
+	}
+	
+	def watchMovie(){
+		amp.turnOn()
+		popper.pop()
+		dvd.turnOn()
+		tv.switchOn()
+		dvd.selectTheMovie()
+		dvd.playTheMovie()
+	}
+	
+	def switchOffMovie(){
+		amp.turnOff()
+		popper.turnOff()
+		dvd.turnOff()
+		tv.turnOff()
+	}
 }
 
 def theater = new HomeTheaterFacade(new Expando(), new Expando(), new Expando(), new Expando())
